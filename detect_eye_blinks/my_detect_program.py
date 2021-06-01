1d3ce3cc0a0bb8af6f16197ad55e0739d69a0454
zhy2on <dhwlgus5940@gmail.com>
2021-06-01 03:40:06 -0900

    6/1-edit eye ar thresh'

--- detect_eye_blinks/my_detect_program.py
+++ detect_eye_blinks/my_detect_program.py
@@ -74,8 +74,8 @@ def eye_aspect_ratio(eye):
 # define two constants, one for the eye aspect ratio to indicate
 # blink and then a second constant for the number of consecutive
 # frames the eye must be below the threshold
-EYE_AR_THRESH = 0.25
-EYE_AR_CONSEC_FRAMES = 3
+EYE_AR_THRESH = 0.28
+EYE_AR_CONSEC_FRAMES = 2
 
 # initialize the frame counters and the total number of blinks
 COUNTER = 0
