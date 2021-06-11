993403fe454320dffe589dc3b3a5c251cdd590a5
zhy2on <dhwlgus5940@gmail.com>
2021-06-10 22:28:33 -0900

    edit alert sound, show number - hunds

--- detect_eye_blinks/my_detect_program.py
+++ detect_eye_blinks/my_detect_program.py
@@ -26,44 +26,44 @@ NUMS =[1,1,1,1,0,1,1,0,1,1,0,1,1,1,1,  # 0
 
 # Displays a single digit (0-9)
 def show_digit(val, xd, yd, r, g, b):
-  offset = val * 15
-  for p in range(offset, offset + 15):
-    xt = p % 3
-    yt = (p-offset) // 3
-    sense.set_pixel(xt+xd, yt+yd, r*NUMS[p], g*NUMS[p], b*NUMS[p])
+    offset = val * 15
+    for p in range(offset, offset + 15):
+        xt = p % 3
+        yt = (p-offset) // 3
+        sense.set_pixel(xt+xd, yt+yd, r*NUMS[p], g*NUMS[p], b*NUMS[p])
 
 # Displays a two-digits positive number (0-99)
 def show_number(val, r, g, b):
-  abs_val = abs(val)
-  tens = abs_val // 10
-  units = abs_val % 10
-  if (abs_val > 9): show_digit(tens, OFFSET_LEFT, OFFSET_TOP, r, g, b)
-  show_digit(units, OFFSET_LEFT+4, OFFSET_TOP, r, g, b)
+    sense.clear()
+    abs_val = abs(val)
+    hunds = (abs_val // 100) % 8
+    tens = (abs_val % 100) / 10
+    units = abs_val % 10
+    if (abs_val > 99):
+        for i in range(1, hunds + 1):
+            sense.set_pixel(8 - i, 0, 255,0,0)
+    if (abs_val > 9): show_digit(tens, OFFSET_LEFT, OFFSET_TOP, r, g, b)
+    show_digit(units, OFFSET_LEFT+4, OFFSET_TOP, r, g, b)
 
 #### detect_py #####
 def eye_aspect_ratio(eye):
     A = dist.euclidean(eye[1], eye[5])
     B = dist.euclidean(eye[2], eye[4])
-
     C = dist.euclidean(eye[0], eye[3])
 
-    # compute the eye aspect ratio
     ear = (A + B) / (2.0 * C)
 
-    # return the eye aspect ratio
     return ear
 
 EYE_AR_THRESH = 0.2
 EYE_AR_CONSEC_FRAMES = 3
+
 # initialize the frame counters and the total number of blinks
 COUNTER = 0
 TOTAL = 0
 
-# initialize timer
 TIMER = 0
 cycle = 60 # default cycle is 1 minute
-
-# initialize break flag
 bflag = 0
 
 # open sound
@@ -71,14 +71,10 @@ pygame.mixer.init()
 bang = pygame.mixer.Sound("alarm/sounds_warning.wav")
 bang.set_volume(1)
 
-# initialize dlib's face detector (HOG-based) and then create
-# the facial landmark predictor
 print("[INFO] loading facial landmark predictor...")
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
-# grab the indexes of the facial landmarks for the left and
-# right eye, respectively
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
@@ -98,16 +94,13 @@ while True:
     if cur-prev >= 1:
         prev = cur
         TIMER = TIMER + 1
-    if TIMER != 0 and (TIMER % 5 == 0): # change to cycle
-        if TOTAL < 1: # change to (cycle / 60) * 12 = cycle * 0.2
-            bang.play()
+    if TIMER != 0 and (TIMER % cycle == 0):
+        if TOTAL < cycle * 0.2: # (cycle / 60) * 12 = cycle * 0.2
+            bang.play(1)
         TOTAL = 0
         time.sleep(1)
         sense.clear()
 
-    # grab the frame from the threaded video file stream, resize
-    # it, and convert it to grayscale
-    # channels)
     frame = vs.read()
     frame = imutils.resize(frame, width=300)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -118,7 +111,6 @@ while True:
     # joystick
     for event in sense.stick.get_events():
         sense.clear()
-        # Check if the joystick was pressed
         if event.action == "pressed":
             if event.direction == "up":
                 if cycle <= 480:
@@ -144,71 +136,61 @@ while True:
 
     # loop over the face detections
     for rect in rects:
-	    # determine the facial landmarks for the face region, then
-        # convert the facial landmark (x, y)-coordinates to a NumPy
-        # arrayc
         shape = predictor(gray, rect)
         shape = face_utils.shape_to_np(shape)
 
-        # extract the left and right eye coordinates, then use the
-        # coordinates to compute the eye aspect ratio for both eyes
         leftEye = shape[lStart:lEnd]
         rightEye = shape[rStart:rEnd]
         leftEAR = eye_aspect_ratio(leftEye)
         rightEAR = eye_aspect_ratio(rightEye)
 
-        # average the eye aspect ratio together for both eyes
         ear = (leftEAR + rightEAR) / 2.0
 
-        # compute the convex hull for the left and right eye, then
-        # visualize each of the eyes
         leftEyeHull = cv2.convexHull(leftEye)
         rightEyeHull = cv2.convexHull(rightEye)
         cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
         cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
 
-        # check to see if the eye aspect ratio is below the blink
-        # threshold, and if so, increment the blink frame counter
         if ear < EYE_AR_THRESH:
             COUNTER += 1
 
-        # otherwise, the eye aspect ratio is not below the blink
-        # threshold
         else:
-            # if the eyes were closed for a sufficient number of
-            # then increment the total number of blinks
             if COUNTER >= EYE_AR_CONSEC_FRAMES:
                 TOTAL += 1
-            # reset the eye frame counter
             COUNTER = 0
 
+        """
         # draw the total number of blinks on the frame along with
         # the computed eye aspect ratio for the frame
-        #cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
-        #            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        #cv2.putText(frame, "EAR: {:.2f}".format(ear), (210, 30),
-        #            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        cv2.putText(frame, "EAR: {:.2f}".format(ear), (210, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        """
         fps.update()
         
     # show digit
     show_number(TOTAL % 100, 0, 80, 0)
     # show fps
     fps.stop()
-    #cv2.putText(frame, "FPS: {:.2f}".format(fps.fps()), (210, 90),
-    #         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-    # show the frame
-    #cv2.imshow("Frame", frame)
-    #key = cv2.waitKey(1) & 0xFF
 
+    """
+    cv2.putText(frame, "FPS: {:.2f}".format(fps.fps()), (210, 90),
+             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    # show the frame
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    # if the `q` key was pressed, break from the loop
+    if (key == ord("q") or bflag):
+        break
+    """
     print("EAR: {:.2f}".format(ear))
-    print("TIME: {:.2f}".format(cur - prev))
+    print("TIME: {:d}".format(TIMER))
     print("FPS: {:.2f}".format(fps.fps()))
     print("TOTAL: {:d}".format(TOTAL))
     print("cycle: {:d}".format(cycle))
 
-    # if the `q` key was pressed, break from the loop
-    #if (key == ord("q") or bflag):
-    #    break
+    
     if bflag:
         break
 
